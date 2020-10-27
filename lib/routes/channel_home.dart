c9a4ce4d42bd5b45841d79aa1d060679757154db
yuki-katayama <katayu810@gmail.com>
2020-10-27 20:11:52 +0900

    チャンネル一覧画面から、クリックしたチャンネルの動画一覧にできるようにした。

--- lib/routes/channel_home.dart
+++ lib/routes/channel_home.dart
@@ -10,7 +10,10 @@ class ChannelHome extends StatefulWidget{
 }
 
 class _ChannelHome extends State {
-  Channel _channel;
+  bool isLoading = true;
+  List<Channel> _channel = [];
+  List list = ['UCEixleMT76xDzoiEb9ZA7XA', 'UCcdd3kS52T9Zyo-SWfj86bA', 'UCL4QAojeGy6CJ9R2PwmlmJQ'
+    ,'UCaG7jufgiw4p5mphPPVbqhw','UC2OetCavEd3foO6GkU5NOyQ','UCC4NkFV-L-vVYD5z_Ei5dUA','UCyk6NETMxuVIWQMv3Vh6Epw'];
 
   @override
   void initState() {
@@ -19,14 +22,19 @@ class _ChannelHome extends State {
   }
 
   _initChannel() async {
-    Channel channel = await APIService.instance
-        .fetchChannel(channelId: 'UCEixleMT76xDzoiEb9ZA7XA');
-    setState(() {
-      _channel = channel;
-    });
+
+    for(int i = 0; i < list.length; i++)
+    {
+      Channel channel = await APIService.instance
+          .fetchChannel(channelId: list[i]);
+      _channel.add(channel);
+    }
+
+    setState(() {});
   }
 
-  _buildProfileInfo() {
+  _buildProfileInfo(int index) {
+
     return ListTile(
       // margin: EdgeInsets.all(20.0),
       // padding: EdgeInsets.all(20.0),
@@ -48,7 +56,7 @@ class _ChannelHome extends State {
             CircleAvatar(
               backgroundColor: Colors.white,
               radius: 35.0,
-              backgroundImage: NetworkImage(_channel.profilePictureUrl),
+              backgroundImage: NetworkImage(_channel[index].profilePictureUrl),
             ),
             SizedBox(width: 12.0),
             Expanded(
@@ -57,7 +65,7 @@ class _ChannelHome extends State {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                    _channel.title,
+                    _channel[index].title,
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 20.0,
@@ -66,7 +74,7 @@ class _ChannelHome extends State {
                     overflow: TextOverflow.ellipsis,
                   ),
                   Text(
-                    '${_channel.subscriberCount} subscribers',
+                    '${_channel[index].subscriberCount} subscribers',
                     style: TextStyle(
                       color: Colors.grey[600],
                       fontSize: 16.0,
@@ -83,7 +91,7 @@ class _ChannelHome extends State {
       onTap: (){
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => ChannelScreen()),
+          MaterialPageRoute(builder: (context) => ChannelScreen(_channel[index])),
         );
       },
     );
@@ -98,10 +106,10 @@ class _ChannelHome extends State {
       body: _channel != null
           ? NotificationListener<ScrollNotification>(
         child: ListView.builder(
-          itemCount: 1,
+          itemCount: list.length,
           itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return _buildProfileInfo();
+            if (index < list.length) {
+              return _buildProfileInfo(index);
             } else{
               return Text("error");
             }
